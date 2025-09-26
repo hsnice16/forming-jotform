c2dc6c7d1a1fa248ead2ced3b938007315fd668c
Himanshu Singh <hsnice16@gmail.com>
2025-09-27 01:39:44 +0530

    feat: add layout effect in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import { useLayoutEffect, useState } from "react";
+
 import { DesktopPage, MobilePage } from "@/components";
 import { isMobileBrowser } from "@/utils";
 
 export default function Page() {
-  if (isMobileBrowser()) {
+  const [isOnMobile, setIsOnMobile] = useState(false);
+
+  useLayoutEffect(() => {
+    if (isMobileBrowser()) {
+      setIsOnMobile(true);
+    }
+  }, []);
+
+  if (isOnMobile) {
     return <MobilePage />;
   }
 
